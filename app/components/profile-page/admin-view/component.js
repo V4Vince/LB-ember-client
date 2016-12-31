c3b0f80f18f714d23d1f4715a0c11ce2790fe5cf
V4Vince <vince.guoo@gmail.com>
2016-12-31 16:57:48 -0500

    Adds edit buttons to profile admin-view
    -Wires up edit buttons to modal to allow update/patch

--- app/components/profile-page/admin-view/component.js
+++ app/components/profile-page/admin-view/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  aboutModalIsOpen: false,
+
+  actions: {
+    editAboutModal () {
+      this.set('aboutModalIsOpen', true);
+    },
+    closeModal () {
+      this.set('aboutModalIsOpen', false);
+    }
+  }
 });
