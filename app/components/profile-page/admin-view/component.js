bee3c76cb5999fefe82d21ab7353fbc0cec3b6f0
V4Vince <vince.guoo@gmail.com>
2017-01-01 16:40:53 -0500

    Creates modal components
    -Moves md-modal from profile-view to  newly created modal-display comp

--- app/components/profile-page/admin-view/component.js
+++ app/components/profile-page/admin-view/component.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //profile <- from profile model
+
+
   aboutModalIsOpen: false,
 
   actions: {
     editAboutModal () {
       this.set('aboutModalIsOpen', true);
     },
-    closeModal () {
-      this.set('aboutModalIsOpen', false);
-    }
   }
 });
