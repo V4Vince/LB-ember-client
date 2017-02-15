875c9eefe1750d12eba522794ebfb3a963fd3ad9
V4Vince <vince.guoo@gmail.com>
2017-02-15 14:02:37 -0500

    Adds to-do comments to bookmark progress

--- app/components/lynn-site/contact-page/component.js
+++ app/components/lynn-site/contact-page/component.js
@@ -4,4 +4,5 @@ export default Ember.Component.extend({
   auth: Ember.inject.service(),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
 
+// To Do: Add observer (maybe) to contacts. If there are no contacts, then display a prompt asking user to enter contact info.
 });
