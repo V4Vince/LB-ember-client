01eb33eece2cf151f1a04ac5826a2f3a73fccd2f
V4Vince <vince.guoo@gmail.com>
2017-02-16 17:35:11 -0500

    Applied fix to handle POSTing invalid objects
    -Adds unloadRecord to contacts route catch promise.

--- app/components/contact-page/admin-tools/new-button/component.js
+++ app/components/contact-page/admin-tools/new-button/component.js
@@ -12,13 +12,21 @@ export default Ember.Component.extend({
     },
 
     addContact () {
-      let contact = this.get('store').createRecord('contact', this.get('contactData'));
-      return contact.save()
-      .then((contact) => {
-        //this will update the store with the newly created record. Otherwise ID will be NULL
-        console.log(contact);
-        // this.get('store').push(contact);
+       let newContact = this.get('store').createRecord('contact', this.get('contactData'));
+       newContact.save()
+      .then(() => {
+        Materialize.toast("Contact saved successfully!", 4000);
+      })
+      .catch(() => {
+        //unloads the record from the store if save() fails.
+        //This prevents any invalid records from being pushed into the store and viewable to user.
+        this.get('store').unloadRecord(newContact);
+        Materialize.toast("Error", 4000);
       });
     },
   }
 });
+
+//To Do:
+//Revise app to reflect convention of data down, action up.
+//This means move all actions into routes and all data processing into components.
--- app/contacts/route.js
+++ app/contacts/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.get('store').findAll('contact');
+    return this.get('store').findAll('contact', {reload: true});
   },
 });
