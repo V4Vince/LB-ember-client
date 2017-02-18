37d769b108726e1cc1b5865e90b27ef791f67166
V4Vince <vince.guoo@gmail.com>
2017-02-17 23:55:02 -0500

    Install Ember-moment & add createdAt to blog model
    -Also scaffold out the editBlog and deleteBlog actions

--- app/blogs/route.js
+++ app/blogs/route.js
@@ -4,4 +4,13 @@ export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('blog');
   },
+  actions: {
+    editBlog: function (){
+      //edit the blog
+    },
+
+    deleteBlog: function(){
+      //delete the blog
+    },
+  }
 });
