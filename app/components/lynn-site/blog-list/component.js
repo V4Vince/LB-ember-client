095279c9885f579c558375044df1b15408dfc28c
V4Vince <vince.guoo@gmail.com>
2017-02-19 17:29:24 -0500

    Fixed delete action
    -Added sendAction to pass the delete action one extra layer

--- app/components/lynn-site/blog-list/component.js
+++ app/components/lynn-site/blog-list/component.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   sortBy: ['createdAt:desc'],
   sortedBlog: Ember.computed.sort('blogs', 'sortBy'),
-  
+
+  //deleteBlog
+
+  actions:{
+    deleteBlog(blog){
+      this.sendAction('deleteBlog', blog);
+    },
+  }
 });
