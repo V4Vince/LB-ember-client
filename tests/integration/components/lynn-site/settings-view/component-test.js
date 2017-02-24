92b91ed7ebeee91ca58254f4b0fc490ca0ab7b77
V4Vince <vince.guoo@gmail.com>
2017-02-24 16:17:30 -0500

    Destroys lynn-site component
    -Lynn-site component is used to group all site view components
    -Also adds seetings-view component

--- tests/integration/components/lynn-site/settings-view/component-test.js
+++ tests/integration/components/lynn-site/settings-view/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('lynn-site', 'Integration | Component | lynn site', {
+moduleForComponent('lynn-site/settings-view', 'Integration | Component | lynn site/settings view', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{lynn-site}}`);
+  this.render(hbs`{{lynn-site/settings-view}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#lynn-site}}
+    {{#lynn-site/settings-view}}
       template block text
-    {{/lynn-site}}
+    {{/lynn-site/settings-view}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
