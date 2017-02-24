d8807da024b38ac026f596eb3363e1ae65a82ee8
V4Vince <vince.guoo@gmail.com>
2017-02-24 18:08:03 -0500

    Adds authentication to settings route
    -checks to see if user is authenticated before entering settings route

--- app/router.js
+++ app/router.js
@@ -11,7 +11,7 @@ Router.map(function () {
   this.route('users');
   this.route('blogs', { path: '/my-blog'});
   this.route('contacts', { path: '/contact-me'});
-  this.route('profile', { path: '/bio'});
+  this.route('profile', { path: '/about-me'});
   this.route('profile-single', { path: '/bio-edit/:profile_id'});
   this.route('settings-page', {path: '/settings'});
   this.route('not-found', {path: '/*path'});
--- app/settings-page/route.js
+++ app/settings-page/route.js
@@ -3,6 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
 
+  beforeModel(){
+    let isAuth = this.get('auth').get('isAuthenticated');
+    if (!isAuth) {
+      console.log(isAuth);
+      this.transitionTo('sign-in')
+      .then(() => {
+        Materialize.toast("Please sign in to access your seetings", 4000);
+      })
+      .catch(() => {
+        Materialize.toast("Error", 4000);
+      });
+    }
+  },
+
   model() {
     return Ember.RSVP.hash({
       profile: this.store.findAll('profile'),
@@ -10,30 +24,31 @@ export default Ember.Route.extend({
     });
   },
 
-setupController(controller, models) {
-  // controller.set('blogs', models.blogs);
-  // controller.set('profile', models.profile);
-  // controller.set('contacts', models.contacts);
+  setupController(controller, models) {
+    // controller.set('blogs', models.blogs);
+    // controller.set('profile', models.profile);
+    // controller.set('contacts', models.contacts);
 
-  // same thing as above but cleaner
-  controller.setProperties(models);
-},
-
-actions: {
-  changePassword (passwords) {
-    this.get('auth').changePassword(passwords)
-    .then(() => this.get('auth').signOut())
-    .then(() => this.transitionTo('sign-in'))
-    .then(() => {
-      this.get('flashMessages')
-      .success('Successfully changed your password!');
-    })
-    .then(() => {
-      Materialize.toast("Password changed successfully!", 4000);
-    })
-    .catch(() => {
-      Materialize.toast("Error", 4000);
-    });
+    // same thing as above but cleaner
+    controller.setProperties(models);
   },
-},
+
+  actions: {
+    changePassword (passwords) {
+      this.get('auth').changePassword(passwords)
+      .then(() => this.get('auth').signOut())
+      .then(() => this.transitionTo('sign-in'))
+      .then(() => {
+        this.get('flashMessages')
+        .success('Successfully changed your password!');
+      })
+      .then(() => {
+        Materialize.toast("Password changed successfully!", 4000);
+      })
+      .catch(() => {
+        Materialize.toast("Error", 4000);
+      });
+    },
+  },
+
 });
