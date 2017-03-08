c6b145dd52de67ad87afc4026a69e180364ae4f2
V4Vince <vince.guoo@gmail.com>
2017-03-08 13:45:19 -0500

    Fixes contact card width not sizing correctly

--- app/components/my-application/component.js
+++ app/components/my-application/component.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 
   user: Ember.computed.alias('auth.credentials.email'),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
-
+  
   actions: {
     logOut(){
       this.sendAction('signOut');
