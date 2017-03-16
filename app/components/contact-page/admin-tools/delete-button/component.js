599f7c6742d6c641c252602cb3182120988884af
V4Vince <vince.guoo@gmail.com>
2017-03-16 14:33:15 -0400

    Adds delete changes to delete button component
    -Forgot to add with the last commit

--- app/components/contact-page/admin-tools/delete-button/component.js
+++ app/components/contact-page/admin-tools/delete-button/component.js
@@ -12,7 +12,7 @@ export default Ember.Component.extend({
       let id = this.get('contact.id');
       //peekRecord won't make a server GET call
       let contact = this.get('store').peekRecord('contact', id);
-      return contact.destroyRecord()
+      return contact.desgit satttroyRecord()
       .then(() => {
         Materialize.toast("Successfully saved new blog post!", 3000);
       })
